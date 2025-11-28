4306a33d84f858e8ea23899d7621ce8901bafae9
Ben Orban <ben@orbanfamily.org>
2025-11-28 10:34:22 -0700

    Initial commit

---
+++ src/main/java/com/orban/abac_prototype/AbacPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.orban.abac_prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbacPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbacPrototypeApplication.class, args);
+	}
+
+}
